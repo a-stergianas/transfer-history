82fc74d7b07692dccd72ee877e37c7a0129a546c
Achilleas <ach.stergianas@gmail.com>
2022-08-01 18:13:10 +0300

    huge update

--- app/src/main/java/com/example/transferhistory/DisplayActivity.kt
+++ app/src/main/java/com/example/transferhistory/DisplayActivity.kt
@@ -4,17 +4,15 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.runtime.Composable
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import java.io.IOException
-import java.util.concurrent.Executors
-import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
-import org.jsoup.select.Elements
+import coil.compose.rememberAsyncImagePainter
 
 class DisplayActivity : ComponentActivity() {
 
@@ -27,132 +25,46 @@ class DisplayActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val url = intent.getStringExtra("url")
+        val year: MutableList<String>  = intent.getSerializableExtra("year") as MutableList<String>
+        val team: MutableList<String>  = intent.getSerializableExtra("team") as MutableList<String>
+        val teamImage: MutableList<String>  = intent.getSerializableExtra("teamImage") as MutableList<String>
+        val countryImage: MutableList<String>  = intent.getSerializableExtra("countryImage") as MutableList<String>
+        val transerType: MutableList<String>  = intent.getSerializableExtra("transerType") as MutableList<String>
+
+        Log.i("TEST", year.toString())
+        Log.i("TEST", team.toString())
+        Log.i("TEST", teamImage.toString())
+        Log.i("TEST", countryImage.toString())
+        Log.i("TEST", transerType.toString())
 
         setContent {
-            CircularIndeterminateProgressBar(isLoading)
-        }
-
-        Log.i("TEST", "Loading...")
-
-        val executor = Executors.newSingleThreadExecutor()
-        executor.execute {
-            var document = Document(null)
-            var document2 = Document(null)
-            var flag : Boolean
-
-            try {
-                document = Jsoup.connect(url!!).get()
-            } catch (e: IOException){
-                Log.i("TEST","Error")
-            }
-
-            var elements : Elements = document.getElementsByClass("tm-player-transfer-history-grid__date")
-            flag = true
-            for(element in elements){
-                if (flag){
-                    flag = false
-                    continue
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Blue)
+            ) {
+                var imageCounter = 0
+                for(i in 0 until year!!.size){
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(Color.White)
+                            .padding(8.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            text = year!!.get(i),
+                            Modifier.width(80.dp)
+                        )
+                        if(team.get(i) != "Without Club" && team.get(i) != "Career break" && team.get(i) != "Ban" && team.get(i) != "Unknown" && team.get(i) != "Retired"){
+                            Image(painter = rememberAsyncImagePainter(teamImage[imageCounter]),
+                                contentDescription = team.get(i),
+                                modifier = Modifier.size(40.dp))
+                            imageCounter += 1
+                        }
+                        Text(text = team!!.get(i))
+                    }
                 }
-                year.add(element.text().takeLast(4))
-            }
-
-            elements = document.getElementsByClass("tm-player-transfer-history-grid__new-club")
-            flag = true
-            for(element in elements){
-                if (flag){
-                    flag = false
-                    continue
-                }
-                //if(element.text() != "Without Club" && element.text() != "Career break" && element.text() != "Ban" && element.text() != "Unknown")
-                team.add(element.text())
-            }
-
-            elements = document.getElementsByClass("tm-player-transfer-history-grid__club-link")
-            flag = false
-            for(element in elements){
-                flag = !flag
-                if(flag)
-                    continue
-
-                try {
-                    document2 = Jsoup.connect("https://www.transfermarkt.com${element.attr("href")}").get()
-                } catch (e: IOException){
-                    Log.i("TEST","Error")
-                }
-
-                var elements2 = document2.getElementsByClass("dataBild").select("img")
-                var imageUrl : String
-                for(element in elements2){
-                    imageUrl = element.attr("src")
-                    teamImage.add(imageUrl)
-                }
-            }
-
-            elements = document.getElementsByClass("tm-player-transfer-history-grid__new-club").select("img")
-            var countryImageUrl : String
-            for(element in elements){
-                countryImageUrl = element.attr("data-src")
-                if(countryImageUrl!="")
-                    countryImage.add(countryImageUrl.drop(52).dropLast(14))
-            }
-
-            elements = document.getElementsByClass("tm-player-transfer-history-grid__fee")
-            flag = true
-            for(element in elements){
-                if (flag){
-                    flag = false
-                    continue
-                }
-                if (element.text() == "End of loan")
-                    transerType.add("END OF LOAN")
-                else if (element.text().contains("loan") or element.text().contains("Loan"))
-                    transerType.add("LOAN")
-                else
-                    transerType.add("TRANSFER")
-            }
-            transerType.removeLast()
-
-            year.reverse()
-            team.reverse()
-            teamImage.reverse()
-            countryImage.reverse()
-            transerType.reverse()
-            
-            for(i in 0..year.size-2){
-                if(year[i] != year[i+1])
-                    year[i] = year[i] + "-" + year[i+1]
-                //Log.i("TEST", year[i] + "-" + year[i+1])
-            }
-            if(team.last().equals("Retired"))
-                //Log.i("TEST", year.last())
-            else{
-                //Log.i("TEST", year.last() + "-")
-                year[year.size-1] = year.last() + "-"
-            }
-
-            Log.i("TEST", year.toString())
-            Log.i("TEST", team.toString())
-            Log.i("TEST", teamImage.toString())
-            Log.i("TEST", countryImage.toString())
-            Log.i("TEST", transerType.toString())
-            isLoading = false
-        }
-    }
-}
-@Composable
-fun CircularIndeterminateProgressBar(isDisplayed: Boolean){
-    if(isDisplayed){
-        Row (
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(50.dp),
-            horizontalArrangement = Arrangement.Center
-        ){
-            if(isDisplayed){
-                CircularProgressIndicator(
-                    color = Color.Red
-                )
             }
         }
     }
--- app/src/main/java/com/example/transferhistory/MainActivity.kt
+++ app/src/main/java/com/example/transferhistory/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.transferhistory
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -19,8 +20,22 @@ import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.ConstraintSet
 import androidx.constraintlayout.compose.Dimension
+import java.io.IOException
+import java.util.concurrent.Executors
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.select.Elements
+import java.io.Serializable
 
 class MainActivity : ComponentActivity() {
+
+    var isLoading = true
+    var year = mutableListOf<String>()
+    var team = mutableListOf<String>()
+    var teamImage = mutableListOf<String>()
+    var countryImage = mutableListOf<String>()
+    var transerType = mutableListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -128,10 +143,117 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier
                         .layoutId("btnGO"),
                     onClick = {
-                        val intent = Intent(this, DisplayActivity::class.java)
-                        if(text.toString().isNotEmpty()){
-                            intent.putExtra("url", text.text)
-                            startActivity(intent)
+                        if(text.text.isNotEmpty()){
+
+                            val executor = Executors.newSingleThreadExecutor()
+                            executor.execute {
+                                Log.i("TEST", "Loading...")
+                                var document = Document(null)
+                                var document2 = Document(null)
+                                var flag : Boolean
+
+                                try {
+                                    document = Jsoup.connect(text.text).get()
+                                } catch (e: IOException){
+                                    Log.i("TEST","Error")
+                                }
+
+                                var elements : Elements = document.getElementsByClass("tm-player-transfer-history-grid__date")
+                                flag = true
+                                for(element in elements){
+                                    if (flag){
+                                        flag = false
+                                        continue
+                                    }
+                                    year.add(element.text().takeLast(4))
+                                }
+
+                                elements = document.getElementsByClass("tm-player-transfer-history-grid__new-club")
+                                flag = true
+                                for(element in elements){
+                                    if (flag){
+                                        flag = false
+                                        continue
+                                    }
+                                    //if(element.text() != "Without Club" && element.text() != "Career break" && element.text() != "Ban" && element.text() != "Unknown")
+                                    team.add(element.text())
+                                }
+
+                                elements = document.getElementsByClass("tm-player-transfer-history-grid__club-link")
+                                flag = false
+                                for(element in elements){
+                                    flag = !flag
+                                    if(flag)
+                                        continue
+
+                                    try {
+                                        document2 = Jsoup.connect("https://www.transfermarkt.com${element.attr("href")}").get()
+                                    } catch (e: IOException){
+                                        Log.i("TEST","Error")
+                                    }
+
+                                    var elements2 = document2.getElementsByClass("dataBild").select("img")
+                                    var imageUrl : String
+                                    for(element in elements2){
+                                        imageUrl = element.attr("src")
+                                        teamImage.add(imageUrl)
+                                    }
+                                }
+
+                                elements = document.getElementsByClass("tm-player-transfer-history-grid__new-club").select("img")
+                                var countryImageUrl : String
+                                for(element in elements){
+                                    countryImageUrl = element.attr("data-src")
+                                    if(countryImageUrl!="")
+                                        countryImage.add(countryImageUrl.drop(52).dropLast(14))
+                                }
+
+                                elements = document.getElementsByClass("tm-player-transfer-history-grid__fee")
+                                flag = true
+                                for(element in elements){
+                                    if (flag){
+                                        flag = false
+                                        continue
+                                    }
+                                    if (element.text() == "End of loan")
+                                        transerType.add("END OF LOAN")
+                                    else if (element.text().contains("loan") or element.text().contains("Loan"))
+                                        transerType.add("LOAN")
+                                    else
+                                        transerType.add("TRANSFER")
+                                }
+                                transerType.removeLast()
+
+                                year.reverse()
+                                team.reverse()
+                                teamImage.reverse()
+                                countryImage.reverse()
+                                transerType.reverse()
+
+                                for(i in 0..year.size-2){
+                                    if(year[i] != year[i+1])
+                                        year[i] = year[i] + "-" + year[i+1]
+                                }
+                                if(team.last().equals("Retired"))
+                                else{
+                                    year[year.size-1] = year.last() + "-"
+                                }
+
+                                Log.i("TEST", year.toString())
+                                Log.i("TEST", team.toString())
+                                Log.i("TEST", teamImage.toString())
+                                Log.i("TEST", countryImage.toString())
+                                Log.i("TEST", transerType.toString())
+
+                                val intent = Intent(this, DisplayActivity::class.java)
+                                intent.putExtra("year", year as Serializable)
+                                intent.putStringArrayListExtra("team", (ArrayList<String>(team)))
+                                intent.putStringArrayListExtra("teamImage", (ArrayList<String>(teamImage)))
+                                intent.putStringArrayListExtra("countryImage", (ArrayList<String>(countryImage)))
+                                intent.putStringArrayListExtra("transerType", (ArrayList<String>(transerType)))
+                                startActivity(intent)
+                            }
+
                         }
                     },
                     colors = ButtonDefaults.buttonColors(
